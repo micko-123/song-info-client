e168f3e6599b73eaf78898595635d8206fd35945
micko-123 <mickyasne123@gmail.com>
2024-05-23 09:45:51 +0300

    bot

---
+++ Bot/bot.js
@@ -0,0 +1,17 @@
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+
+const url = "https://song-info-client.vercel.app/";
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply("Welcome"), {
+  reply_markup: {
+    keyboard: [[{ text: "web_app", web_app: { url } }]],
+  },
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
